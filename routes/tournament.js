72c236af46d823c31f4d24fadc2ff079a3a84cc1
TrevorJSanders <TrevorJSanders19@gmail.com>
2025-06-09 19:43:36 -0500

    simplifying server to test

--- routes/tournament.js
+++ routes/tournament.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Tournament = require('../models/tournamentSchema');
+const Tournament = require('../models/Tournament');
 
 // GET /api/tournaments - Get all tournaments, sorted by startDatetime descending
 router.get('/', async (req, res) => {
   try {
-    const tournaments = await Tournament.find().sort({ startDatetime: -1 });
+    const tournaments = await Tournament.find();
     res.json(tournaments);
   } catch (err) {
     console.error('Error fetching tournaments:', err);
