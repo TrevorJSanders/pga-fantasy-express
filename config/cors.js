6a2027c5e13db2fbaa4a41682b9444efdc038d9e
TrevorJSanders <TrevorJSanders19@gmail.com>
2025-06-11 21:14:44 -0500

    adding localhost:5173

--- config/cors.js
+++ config/cors.js
@@ -7,7 +7,8 @@ const corsOptions = {
     'http://localhost:3000',  // Local React development
     'http://localhost:3001',  // Local server
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5173',  // Add this - Vite dev server
+    'http://localhost:5173',  // Add this too - alternative localhost
     API_ENDPOINT_URI,             // Your Railway production URL
     // Add any other frontend URLs you need
   ],
