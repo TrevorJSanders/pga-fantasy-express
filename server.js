2390db14f0ce698ff9dd6c6b93713718983b8ef9
TrevorJSanders <TrevorJSanders19@gmail.com>
2025-06-18 20:51:19 -0500

    MONGODB not MONGO

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const wss = new WebSocket.Server({ server });
 const pubsub = new EventEmitter();
 
 // MONGODB CHANGE STREAM SETUP
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
